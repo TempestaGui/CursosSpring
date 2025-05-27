6f8793d1aea83bd917f6949ed1648678fa6ffd4c
DevGuilhermeTempesta <guilhermeftempesta@gmail.com>
2025-05-26 23:16:34 -0300

    criando properties, aluno, orm

---
+++ src/main/java/com/TempestaGui/CursoSpring/entities/Aluno.java
@@ -0,0 +1,121 @@
+package com.TempestaGui.CursoSpring.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String matricula;
+    private Integer idade;
+    private String curso;
+    private Integer semestre;
+    private String email;
+    private String telefone;
+    private String cidade;
+
+    public Aluno() {}
+
+    public Aluno(Long id, String nome, String matricula, Integer idade, String curso, Integer semestre,
+                 String email, String telefone, String cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+        this.curso = curso;
+        this.semestre = semestre;
+        this.email = email;
+        this.telefone = telefone;
+        this.cidade = cidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
+    }
+
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+}
