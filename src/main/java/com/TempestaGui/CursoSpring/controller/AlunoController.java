4c9c9180d520fd3848f27abae9c7be6b4751a0f7
DevGuilhermeTempesta <guilhermeftempesta@gmail.com>
2025-06-22 17:50:30 -0300

    criando o get do aluno pelo id

--- src/main/java/com/TempestaGui/CursoSpring/controller/AlunoController.java
+++ src/main/java/com/TempestaGui/CursoSpring/controller/AlunoController.java
@@ -5,6 +5,7 @@ import com.TempestaGui.CursoSpring.entities.Aluno;
 import com.TempestaGui.CursoSpring.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class AlunoController {
     @GetMapping
     public List<AlunoMinDto> findAll(){
         List<AlunoMinDto> result = alunoService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public AlunoMinDto findById(@PathVariable Long id){
+        AlunoMinDto result = alunoService.findAlunoById(id);
         return result;
     }
 }
--- src/main/java/com/TempestaGui/CursoSpring/services/AlunoService.java
+++ src/main/java/com/TempestaGui/CursoSpring/services/AlunoService.java
@@ -3,6 +3,7 @@ import java.util.List;
 import com.TempestaGui.CursoSpring.Dto.AlunoMinDto;
 import com.TempestaGui.CursoSpring.Repository.AlunoRepository;
 import com.TempestaGui.CursoSpring.entities.Aluno;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,12 @@ public class AlunoService {
     @Autowired
     private AlunoRepository alunoRepository;
 
+    @Transactional(readOnly = true)
+    public AlunoMinDto findAlunoById(Long id) {
+        Aluno result = alunoRepository.findById(id).get();
+        return new AlunoMinDto(result);
+    }
+    @Transactional(readOnly = true)
     public List<AlunoMinDto> findAll(){
         List<Aluno> result = alunoRepository.findAll();
         List<AlunoMinDto> dto = result.stream().map(x -> new AlunoMinDto(x)).toList();
